c3ea9631e012d11f8d82236ee9b5c374623b26f5
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-04-11 21:48:09 -0500

    Added multiplayer

--- ui/MultiplayerUI.gd
+++ ui/MultiplayerUI.gd
@@ -25,12 +25,23 @@ var port : int = 3000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"%HostPopup".popup()
+	get_tree().connect("network_peer_connected", self, "_on_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_on_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_on_client_connected_ok")
+	get_tree().connect("connection_failed", self, "_on_client_connected_fail")
+	get_tree().connect("server_disconnected", self, "_on_server_disconnected")
 
 ## Chatting ##
 # Sends a message to the chat client.
-func send_message(message : String, is_me_style : bool = false):
-	message_history.append(Message.new(is_me_style, username, message))
+remote func send_message(message : String, is_me_style : bool = false, 
+		being_synced : bool = false, user : String = username):
+	
+	message_history.append(Message.new(is_me_style, user, message))
 	update_text_box()
+	
+	# Sync message with everyone else if we're sending it
+	if not being_synced:
+		rpc("send_message", message, is_me_style, true, username)
 
 # Updates the textbox with new info.
 func update_text_box():
@@ -43,10 +54,21 @@ func update_text_box():
 # Creates a multiplayer server
 func join_as_host():
 	print("Joining as host...")
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(port, 2)
+	get_tree().network_peer = peer
 
 # Joins a multiplayer server at the given address `addr` as a client
 func join_as_client(addr : String):
-	print("Joining as client...")
+	print("Joining as client!")
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(addr, port)
+	get_tree().network_peer = peer
+
+# Disconnects from the network.
+func _handle_disconnect():
+	get_tree().network_peer = null
+	send_message("disconnected from the server.", true)
 
 ## Handlers ##
 # Handle pressing enter or the submit button in the UsernamePopup
@@ -73,6 +95,22 @@ func _handle_port_input(is_host : bool, new_port : String):
 	# Close this popup and put in the other one
 	$"%HostPopup".hide()
 	$"%UsernamePopup".popup()
+
+## Multiplayer ##
+func _on_player_connected(id):
+	print("Player with ID " + String(id) + " connected!")
+
+func _on_player_disconnected(id):
+	print("Player with ID " + String(id) + " disconnected!")
+
+func _on_client_connected_ok():
+	print("Connected to server successfully!")
+
+func _on_client_connected_fail():
+	print("Failed to connect to server!")
+
+func _on_server_disconnected():
+	send_message("disconnected.", true, true, "Server")
 
 ## Chat Windows ##
 # Connect to when the user presses enter when typing into the chat window
